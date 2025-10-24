11ee0c10bdd7873bd1c8fa8e6700f3761d5a57cf
OscarB <oscartic@gmail.com>
2025-10-24 00:40:59 -0300

    chore (controller): adds base controller and health endpoint
    Sets up a base controller with standard response formats.
    
    Introduces a health endpoint to provide basic service status
    information.
    
    Creates a product controller and service to handle the creation,
    retrieval, and listing of products.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+require "json"
+require_relative "base_controller"
+require_relative "../services/product_service"
+
+class ProductsController < BaseController
+    def initialize(app = nil)
+        @product_service = ProductService.new
+    end
+    
+    def call(env)
+        request = Rack::Request.new(env)
+        
+        case [request.request_method, request.path_info]
+        when ["POST", "/"], ["POST", "/create"]
+          create(env)
+        when ["GET", "/"], ["GET", "/list"]
+          all
+        when ["GET", request.path_info]
+          if request.path_info =~ /^\/(\d+)$/
+            find_product($1.to_i)
+          else
+            not_found_response
+          end
+        else
+          not_found_response
+        end
+    end
+
+    private
+
+    def create(env)
+        attrs = env["parsed_body"]
+        return bad_request_response(["body is required"]) if attrs.nil? || attrs.empty?
+
+        product, errors = @product_service.create(attrs)
+        return unprocessable_entity_response(errors) if errors
+
+        json_response(product, 201)
+    end
+
+    def all
+        products = @product_service.all
+        json_response(products)
+    end
+
+    def find_product(id)
+        product = @product_service.find(id)
+        return not_found_response unless product
+        
+        json_response(product.to_h)
+    end
+end
